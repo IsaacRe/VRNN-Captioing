ca8250a3390a850c146020d67cde043314670ac3
Isaac Rehg <irehg@ucsd.edu>
2018-03-05 10:57:22 -0800

    setup script to install pycoco packages to conda env

--- test.py
+++ test.py
@@ -13,7 +13,7 @@ from build_vocab import Vocabulary
 from model import EncoderCNN, DecoderRNN
 from data_loader import get_loader
 from collections import Counter
-from pycocotools.coco import COCO
+from pycocotoolscap.coco import COCO
 from pycocoevalcap.eval import COCOEvalCap
 from torch.nn.utils.rnn import pack_padded_sequence
 
